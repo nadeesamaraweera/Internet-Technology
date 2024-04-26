d62635a8620b55933550f556db454e44954ae3bf
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-04-26 19:27:08 +0530

    complete exercise 5

--- js/Lesson-js.js
+++
@@ -1,4 +0,0 @@
-console.log("Add : ", 100 + 100);
-console.log("Sub : ", 100 - 100);
-console.log("Mul : ", 100 * 100);
-console.log("Div : ", 100 / 100);
--- js/VariableHoisting.js
+++
@@ -1,9 +0,0 @@
-/** Java script interpreter take all variable declaration and put it on top of the code. Interpreter only take the declaration part.
- * After drag it to the top interpreter initialize the variable as 'undefined'. This is only happen if we declare variable using 'var'. */
-
-/** If we declare as a local variable in a function interpreter take the variable declaration to the top of the function or to the top of the block. */
-
-/** With 'let' and 'const', hoisting is done by js interpreter but interpreter won't initialize them. */
-
-console.log(x);
-var x = 100;
