4c151a6b98eaa6d54705b6f9e98b199ba64ea1a9
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-04-27 10:12:00 +0530

    complete set Timeout

--- js/Timers.js
+++ js/Timers.js
@@ -1,10 +1,26 @@
-//1.setTimeout
+//1.setTimeout -- executes a function once after a specific delay
 
 
 //setTimeout(callback , 5000)
 
-setTimeout(function () {
-     console.log("Hello........")
+// setTimeout(function () { //use Anonymous  function
+//      console.log("Hello........")
+// } ,5000)
+
+// setTimeout( () => {   // use arrow function
+//     console.log("Hello........")
+// } ,5000)
+
+
+let timeout = setTimeout( () => {  
+    console.log("Hello........")
 } ,5000)
 
+clearTimeout(timeout)
+
+
+
+
+
 //2.setInterval
+
