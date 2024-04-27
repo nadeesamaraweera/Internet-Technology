e847c6ffcb6f6408a87eb8453ff2528f8d29da20
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-04-27 10:28:03 +0530

    complete

--- js/Timers.js
+++ js/Timers.js
@@ -3,14 +3,19 @@
 
 //setTimeout(callback , 5000)
 
+//------------------------
+
 // setTimeout(function () { //use Anonymous  function
 //      console.log("Hello........")
 // } ,5000)
+
+//-------------------------
 
 // setTimeout( () => {   // use arrow function
 //     console.log("Hello........")
 // } ,5000)
 
+//-------------------------
 
 let timeout = setTimeout( () => {  
     console.log("Hello........")
@@ -27,17 +32,39 @@ console.log("=====================")
 
 //setInterval(callback , interval)
 
+//--------------------------
+
 // setInterval(() => {
 //     console.log("Hey")
 // } , 5000);
 
+//----------------------------
 
-let interval = setInterval(() => {
-    console.log("Hey")
-} , 5000);
+// let interval = setInterval(() => {
+//     console.log("Hey")
+// } , 5000);
 
-//cancel the  timeout
-clearInterval(interval)
+// //cancel the  timeout
+// clearInterval(interval) 
+
+//----------------------------
+
+// let greet = () =>{
+//     console.log("Hello world")
+
+// }
+
+// let interval = setInterval(greet,1000)
+
+//-------------------------------
+
+
+
+let interval = setInterval(greet,1000) // function hoisting
+let greet = () =>{
+    console.log("Hello world")
+
+}
 
 console.log("=====================")
 
