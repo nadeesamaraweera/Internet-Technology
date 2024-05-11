d2c079669fafc082245f8adeee9ae0343394a4cf
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-05-11 11:48:53 +0530

    EventJQuery.js

---
+++ js/EventJQuery.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+
+    $('#box').on('mouseover' , function(){
+        $('#box').css('background-color', 'green')
+    })
+
+
+    $('#box').on('mouseout' , function(){
+        $('#box').css('background-color', 'blue')
+    })
+})
+
+
+
+
+
+
+
+  
